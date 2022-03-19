c8b67dd3fda5638af402188ff56e49ab72d92118
Easin Arafat <arafatmrx@gmail.com>
2022-03-19 09:51:54 +0600

    input test

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 var a_hour = window.prompt("Enter the Hour:");
 var a_minute = window.prompt("Enter the Minutes:");
 var a_second = window.prompt("Enter the Second:");
+
+document.querySelector(".alarm").textContent =
+  a_hour + ":" + a_minute + ":" + a_second;
