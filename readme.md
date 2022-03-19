ce5f4f121be515ed21a624f99cf85212d376695a
Easin Arafat <arafatmrx@gmail.com>
2022-03-19 10:17:09 +0600

    get time

---
+++ readme.md
@@ -0,0 +1,4 @@
+## learned from here
+
+- getting time get methods
+- setTimeout(fn,time_ms)
--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var a_hour = window.prompt("Enter the Hour:");
-var a_minute = window.prompt("Enter the Minutes:");
-var a_second = window.prompt("Enter the Second:");
+// var a_hour = window.prompt("Enter the Hour:");
+// var a_minute = window.prompt("Enter the Minutes:");
+// var a_second = window.prompt("Enter the Second:");
 
-document.querySelector(".alarm").textContent =
-  a_hour + ":" + a_minute + ":" + a_second;
+// document.querySelector(".alarm").textContent =
+//   a_hour + ":" + a_minute + ":" + a_second;
 
 function current_date() {
   const d = new Date();
-  let hours = d.getHours;
-  let minutes = d.getMinutes;
-  let second = d.getSeconds;
+  let hours = d.getHours();
+  let minutes = d.getMinutes();
+  let seconds = d.getSeconds();
+
+  document.querySelector(".alarm").textContent =
+    hours + ":" + minutes + ":" + seconds;
+
+  setTimeout(current_date, 100);
 }
+current_date();
